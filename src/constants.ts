540802caab7042010645e0feb000db261550f9b4
blandedbeans <danielgraves3301@gmail.com>
2022-07-08 14:55:46 -0600

    Co-authored-by: Justin Tan <Justin.Tan@ecssen.ca>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 import { chain } from 'wagmi'
-
 // Environments
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
